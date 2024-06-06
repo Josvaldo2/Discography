6b1f9c23ba365cbc19d59e2b0ce1e4fc391b0bf0
Josvaldo2 <59576112+Josvaldo2@users.noreply.github.com>
2024-06-06 10:28:51 -0300

    Update

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
-import questions from "./questions.json"
+import questions from "./questions"
 
 let titleScreen = document.getElementById("titleScreen");
 let questionScreen = document.getElementById("questionScreen");
 let running = false;
 let currentQuestion = 0;
 let guesses = 0;
+let = questionHeader
+
+let currentQuestionElement = document.getElementById("currentQuestion");
 
 function changeQuestion() {
+    currentQuestionElement.innerText = currentQuestion;
+    let questionN = Math.round(Math.random()*(questions.length-1))
 
+
+}
+
+export function submit(x) {
+    currentQuestion++;
+    changeQuestion();
 }
 
 function start() {
     running = true;
+    changeQuestion();
     titleScreen.style.opacity = 0;
     questionScreen.style.opacity = 1;
     titleScreen.style.zIndex = -1
     questionScreen.style.zIndex = 1
     console.log(questions)
 }
+
+document.getElementById("options").querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", submit)
+})
 
 document.getElementById("start").addEventListener("click", start);
---
+++ questions.js
@@ -0,0 +1,106 @@
+export default [
+    {
+      "Which of these songs was written by Eminem?": {
+        "Beautiful": false,
+        "It Was a Good Day": true,
+        "Without Me": true,
+        "Samba de Janeiro": false
+      }
+    },
+    {
+      "Who produced the album 'The Marshall Mathers LP' for Eminem?": {
+        "Dr. Dre": true,
+        "Timbaland": false,
+        "Swizz Beatz": false,
+        "Scott Storch": false
+      }
+    },
+    {
+      "In what year did Jay-Z release his debut studio album 'Reasonable Doubt'?": {
+        "1990": false,
+        "1996": true,
+        "1998": false,
+        "2001": false
+      }
+    },
+    {
+      "What is the name of the hit single from OutKast's album 'Speakerboxxx/The Love Below'?": {
+        "Hey Ya": true,
+        "Ms. Jackson": false,
+        "B.O.B": false,
+        "Roses": false
+      }
+    },
+    {
+      "Which of these artists is known for their song 'Lose Yourself'?": {
+        "Eminem": true,
+        "Jay-Z": false,
+        "50 Cent": false,
+        "Kanye West": false
+      }
+    },
+    {
+      "Which of these songs won the Grammy Award for Song of the Year in 2019?": {
+        "This Is America by Childish Gambino": true,
+        "God’s Plan by Drake": false,
+        "Perfect by Ed Sheeran": false,
+        "Havana by Camila Cabello featuring Young Thug": false
+      }
+    },
+    {
+      "What is the name of the album that includes the song 'Hotline Bling' by Drake?": {
+        "Views": true,
+        "Nothing Was the Same": false,
+        "Take Care": false,
+        "Scorpion": false
+      }
+    },
+    {
+      "Who is the artist behind the song 'Crank That'?": {
+        "Soulja Boy Tell 'Em": true,
+        "Lil Wayne": false,
+        "T.I.": false,
+        "Young Jeezy": false
+      }
+    },
+    {
+        "Which song made Jay-Z famous?": {
+        "99 Problems": false,
+        "Big Pimpin'": false,
+        "Hard Knock Life (Ghetto Anthem)": false,
+        "The Blueprint": true
+        }
+    },
+    {
+        "In what year did Eminem release his breakthrough album 'The Slim Shady LP'?": {
+        "1999": true,
+        "2000": false,
+        "2001": false,
+        "2002": false
+        }
+    },
+    {
+        "Who wrote and performed the song 'Ninjas in Paris'?": {
+        "Kanye West and Jay-Z": true,
+        "Jay-Z and Nas": false,
+        "Kanye West and Lil Wayne": false,
+        "Drake and Kanye West": false
+        }
+    },
+    {
+        "What is the title of the song that features Snoop Dogg rapping about his love for marijuana?": {
+        "Gin and Juice": true,
+        "Drop It Like It's Hot": false,
+        "Ain't No Fun": false,
+        "Who Am I (What's My Name)?": false
+        }
+    },
+    {
+        "What is the name of the album that includes the hit single 'Uptown Funk' by Mark Ronson featuring Bruno Mars?": {
+        "Uptown Special": true,
+        "Dua Lipa": false,
+        "21 Pilots": false,
+        "The Weeknd": false
+        }
+    }
+]
